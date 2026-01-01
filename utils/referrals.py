f7a8cb22ea4a4c5ab3d343742fdab5ff5f973530
eslam011172-netizen <eslam011172@gmail.com>
2026-01-01 15:06:51 +0100

    Update referrals.py

--- utils/referrals.py
+++ utils/referrals.py
@@ -1,1 +1,11 @@
+from utils.users import get_user, add_user, add_balance
 
+
+def add_referral(referrer_id, new_user_id):
+    referrer = get_user(referrer_id)
+
+    if not referrer:
+        add_user(referrer_id)
+
+    # مكافأة الدعوة
+    add_balance(referrer_id, 5)

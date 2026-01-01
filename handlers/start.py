5156cd767fe536107e880b26d1df8e18e5560143
eslam011172-netizen <eslam011172@gmail.com>
2026-01-01 15:30:37 +0100

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,1 +1,14 @@
-bot_vip/handlers/start.py
+Enterfrom telebot import types
+from utils.users import add_user
+
+def start_handler(bot, message):
+    add_user(message.from_user.id)
+
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.add("رصيدي 💰", "دعوة أصدقاء 👥")
+
+    bot.send_message(
+        message.chat.id,
+        "أهلاً بيك 👋",
+        reply_markup=markup
+    )

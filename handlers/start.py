192e605526c6aca5370f58258bff3317ebe129d1
eslam011172-netizen <eslam011172@gmail.com>
2026-01-01 16:15:34 +0100

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,8 +1,25 @@
 from telebot import types
 from utils.users import add_user
+from utils.force_subscribe import check_sub
+from config import FORCE_CHANNEL
 
 def start_handler(bot, message):
-    add_user(message.from_user.id)
+    user_id = message.from_user.id
+
+    if not check_sub(bot, user_id, FORCE_CHANNEL):
+        markup = types.InlineKeyboardMarkup()
+        markup.add(
+            types.InlineKeyboardButton("📢 اشترك في القناة", url=f"https://t.me/{FORCE_CHANNEL.replace('@','')}"),
+            types.InlineKeyboardButton("✅ تحقق", callback_data="check_sub")
+        )
+        bot.send_message(
+            message.chat.id,
+            "🚫 لازم تشترك في القناة الأول",
+            reply_markup=markup
+        )
+        return
+
+    add_user(user_id)
 
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     markup.add("رصيدي 💰", "دعوة أصدقاء 👥")

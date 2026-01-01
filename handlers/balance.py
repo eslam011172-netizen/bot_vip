2be13b978ee81e792e8f1d77ee3bda8fccd9f5b1
eslam011172-netizen <eslam011172@gmail.com>
2026-01-01 15:01:46 +0100

    Update balance.py

--- handlers/balance.py
+++ handlers/balance.py
@@ -1,1 +1,17 @@
+from utils.users import get_user, add_user
 
+
+def balance_handler(bot):
+    @bot.message_handler(commands=['balance'])
+    def balance(message):
+        user_id = message.from_user.id
+        user = get_user(user_id)
+
+        if not user:
+            add_user(user_id)
+            balance = 0
+        else:
+            balance = user.get("balance", 0)
+
+        text = f"💰 رصيدك الحالي: {balance}"
+        bot.send_message(message.chat.id, text)
